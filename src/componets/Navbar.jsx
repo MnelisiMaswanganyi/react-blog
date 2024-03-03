cb5a912674470be0078dbd549ec4b783a6343e10
Jason <murangira99@gmail.com>
2024-03-03 05:11:56 +0200

    added const nav bar

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -2,3 +2,6 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/authContext";
 import Logo from "../img/logo.png";
+
+const Navbar = () => {
+  const { currentUser, logout } = useContext(AuthContext);
