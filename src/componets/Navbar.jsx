90047fb7904b0d2a7735292b7cede665008aba73
Jason <murangira99@gmail.com>
2024-03-03 05:12:45 +0200

    added a return div

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -5,3 +5,12 @@ import Logo from "../img/logo.png";
 
 const Navbar = () => {
   const { currentUser, logout } = useContext(AuthContext);
+
+   return (
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          <Link to="/">
+          <img src={Logo} alt="" />
+          </Link>
+        </div>
