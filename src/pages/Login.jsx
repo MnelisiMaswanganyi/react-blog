f30ba440cdceb7a05b4283b7e779781b052ae6eb
Jason <murangira99@gmail.com>
2024-03-03 05:43:13 +0200

    added const handles

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,3 +3,20 @@ import React, { useState } from "react";
 import { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/authContext";
+
+const Login = () => {
+    const [inputs, setInputs] = useState({
+      username: "",
+      password: "",
+    });
+    const [err, setError] = useState(null);
+  
+    const navigate = useNavigate();
+  
+    const { login } = useContext(AuthContext);
+  
+  
+    const handleChange = (e) => {
+      setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    };
+  
