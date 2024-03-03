e83846afcda6e4e826f9b60aca737d709e3a938d
Jason <murangira99@gmail.com>
2024-03-03 05:21:29 +0200

    added export const authcontext

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,2 +1,9 @@
 import axios from "axios";
 import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthContexProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || null
+  );
