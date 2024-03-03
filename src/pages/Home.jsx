5a451b12de3b5e7210b5e90bca8feb30d40b5a84
Jason <murangira99@gmail.com>
2024-03-03 05:37:07 +0200

    added use effect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,3 +3,20 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import axios from "axios";
+
+const Home = () => {
+    const [posts, setPosts] = useState([]);
+  
+    const cat = useLocation().search
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const res = await axios.get(`/posts${cat}`);
+          setPosts(res.data);
+        } catch (err) {
+          console.log(err);
+        }
+      };
+      fetchData();
+    }, [cat]);
