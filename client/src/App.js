9014e8cfbc8c39bcb341986bf70c52440145932d
Mnelisi Maswanganyi <142810215+MnelisiMaswanganyi@users.noreply.github.com>
2024-03-03 21:32:50 +0200

    app update

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,34 @@ import {
     createBrowserRouter,
     RouterProvider,
   } from "react-router-dom";
+  import register from "./pages/register"
+  import login from "./pages/login"
+  import single from "./pages/single"
+  import write from "./pages/write"
+  import home from "./pages/home"
 
   const router = createBrowserRouter([
     {
       path: "/",
       element: <div>This is home!</div>,
     },
+    {
+        path: "/register",
+        element: <div>This is Register!</div>,
+      },
+      {
+        path: "/login",
+        element: <div>This is login!</div>,
+      },
+      {
+        path: "/single",
+        element: <div>This is single!</div>,
+      },
+      {
+        path: "/write",
+        element: <div>This is write!</div>,
+      },
+    
   ]);
 
   function  App() {
