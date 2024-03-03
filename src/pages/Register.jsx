7436eef5a68b73795a768fadda70b8a2ce2e02e0
Jason <murangira99@gmail.com>
2024-03-03 06:53:51 +0200

    added const register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,3 +2,17 @@ import React from "react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+
+const Register = () => {
+    const [inputs, setInputs] = useState({
+      username: "",
+      email: "",
+      password: "",
+    });
+    const [err, setError] = useState(null);
+  
+    const navigate = useNavigate();
+  
+    const handleChange = (e) => {
+      setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    };
